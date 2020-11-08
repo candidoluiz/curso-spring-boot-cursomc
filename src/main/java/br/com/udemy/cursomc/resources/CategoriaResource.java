1864822b997a5e35194ef149a889483c5b790273
luiz <luiz.fernando360@gmail.com>
2020-11-08 03:01:47 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/br/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/udemy/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import br.com.udemy.cursomc.domain.Categoria;
 import br.com.udemy.cursomc.dto.CategoriaDTO;
 import br.com.udemy.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -52,4 +53,16 @@ public class CategoriaResource {
         List<CategoriaDTO> categoriaDTOS = categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(categoriaDTOS);
     }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+            @RequestParam(value = "oderBy", defaultValue = "nome")String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC")String direction){
+        Page<Categoria> categorias = categoriaService.findPage(page,linesPerPage,orderBy,direction);
+        Page<CategoriaDTO> categoriaDTOS = categorias.map(obj -> new CategoriaDTO(obj));
+        return ResponseEntity.ok().body(categoriaDTOS);
+    }
+
 }
--- src/main/java/br/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/udemy/cursomc/services/CategoriaService.java
@@ -4,9 +4,11 @@ import br.com.udemy.cursomc.domain.Categoria;
 import br.com.udemy.cursomc.repositories.CategoriaRepository;
 import br.com.udemy.cursomc.services.exceptions.DataIntegrityException;
 import br.com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -46,5 +48,10 @@ public class CategoriaService {
 
     public List<Categoria> findAll(){
         return categoriaRepository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage,Sort.Direction.valueOf(direction),orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
